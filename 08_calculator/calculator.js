3554c2a90bdebec445e4bc071eeeeff59e3c870e
Bernard Muli <bernardnthiwa@gmail.com>
2023-01-19 13:39:55 +0300

    part 4

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(num1,num2) {
+	return num1 + num2
+};
+
+const subtract = function(num1,num2) {
+  return num1 - num2
 	
 };
 
-const subtract = function() {
+const sum = function(arrayOfNum) {
+    return arrayOfNum.reduce((cur,acc) =>{
+      return cur + acc
+    },0)
+  }
+
+const multiply = function(arrayOfNum) {
+  return arrayOfNum.reduce((cur,acc) =>{
+    return cur * acc
+  })
+
+};
+
+const power = function(num1,num2) {
+  return Math.pow(num1,num2)
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  let factorial = 1
+  if(num===0){
+    factorial = 1
+  }else{
+    for(let i =1; i<num+1; i++){
+      factorial *= i
+    }
+  }
+  return factorial
 	
 };
 
